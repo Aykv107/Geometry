aad0570ec848bb3e622b53b73c9da889ac261bf0
Aykv107 <48999217+Aykv107@users.noreply.github.com>
2021-02-24 22:01:25 -0300

    Update rotational.lua

--- rotational.lua
+++ rotational.lua
@@ -1,35 +1,88 @@
 velocities = {}
 colors = {}
 
+function rgb_hex(r, g, b)
+	local base16 = {
+		[0] = "0",
+		[1] = "1",
+		[2] = "2",
+		[3] = "3",
+		[4] = "4",
+		[5] = "5",
+		[6] = "6",
+		[7] = "7",
+		[8] = "8",
+		[9] = "9",
+		[10] = "A",
+		[11] = "B",
+		[12] = "C",
+		[13] = "D",
+		[14] = "E",
+		[15] = "F"
+	}
+
+	local _r1 = math.floor(r / 16)
+	local _r2 = ((r / 16) - _r1) * 16
+
+	local _g1 = math.floor(g / 16)
+	local _g2 = ((g / 16) - _g1) * 16
+
+	local _b1 = math.floor(b / 16)
+	local _b2 = ((b / 16) - _b1) * 16
+
+	return base16[_r1] ..base16[_r2] .. base16[_g1] .. base16[_g2] .. base16[_b1] .. base16[_b2] 
+end
+
 --------------------------
 
 velocity = 0
-velocityProportion = 0.01
+velocityProportion = 0.1
 
 --------------------------
 
-color = 0x000000
-colorProportion = 0x00000b
+r = 0
+g = 0
+b = 0
+
+rp = 9
+gp = 0
+bp = 12
 
 --------------------------
 
 for i = 1, 20 do
+	--if i % 2 == 0 then
 	velocities[i] = velocity
+	--else
+	--velocities[i] = -velocity
+	--end
 
 	velocity = velocity - velocityProportion
 end
 
 for i = 1, 21 do
-	local colorConfig = tostring(color)
+	colors[i] = rgb_hex(r, g, b)
 
-	colors[i] = colorConfig
-
-	color = color + colorProportion
+	r = math.abs(r + rp)
+	g = math.abs(g + gp)
+	b = math.abs(b + bp)
 end
 
 map = [[<C><P MEDATA=";;;0,1;-0;0::0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31:1-"/><Z><S><S T="0" X="400" Y="387" L="1650" H="10" P="0,0,0.3,0.2,0,0,0,0"/><S T="12" X="400" Y="200" L="1600" H="1600" P="0,0,0,0,0,0,100,0" o="]]..colors[1]..[[" c="4"/><S T="12" X="400" Y="200" L="1525" H="1525" P="1,0,0,0,5,0,100,0" o="]]..colors[2]..[[" c="4"/><S T="12" X="400" Y="200" L="1450" H="1450" P="1,0,0,0,10,0,100,0" o="]]..colors[3]..[[" c="4"/><S T="12" X="400" Y="200" L="1375" H="1375" P="1,0,0,0,15,0,100,0" o="]]..colors[4]..[[" c="4"/><S T="12" X="400" Y="200" L="1300" H="1300" P="1,0,0,0,20,0,100,0" o="]]..colors[5]..[[" c="4"/><S T="12" X="400" Y="200" L="1225" H="1225" P="1,0,0,0,25,0,100,0" o="]]..colors[6]..[[" c="4"/><S T="12" X="400" Y="200" L="1150" H="1150" P="1,0,0,0,30,0,100,0" o="]]..colors[7]..[[" c="4"/><S T="12" X="400" Y="200" L="1075" H="1075" P="1,0,0,0,35,0,100,0" o="]]..colors[8]..[[" c="4"/><S T="12" X="400" Y="200" L="1000" H="1000" P="1,0,0,0,40,0,100,0" o="]]..colors[9]..[[" c="4"/><S T="12" X="400" Y="200" L="925" H="925" P="1,0,0,0,45,0,100,0" o="]]..colors[10]..[[" c="4"/><S T="12" X="400" Y="200" L="850" H="850" P="1,0,0,0,50,0,100,0" o="]]..colors[11]..[[" c="4"/><S T="12" X="400" Y="200" L="775" H="775" P="1,0,0,0,55,0,100,0" o="]]..colors[12]..[[" c="4"/><S T="12" X="400" Y="200" L="700" H="700" P="1,0,0,0,60,0,100,0" o="]]..colors[13]..[[" c="4"/><S T="12" X="400" Y="200" L="625" H="625" P="1,0,0,0,65,0,100,0" o="]]..colors[14]..[[" c="4"/><S T="12" X="400" Y="200" L="550" H="550" P="1,0,0,0,70,0,100,0" o="]]..colors[15]..[[" c="4"/><S T="12" X="400" Y="200" L="475" H="475" P="1,0,0,0,75,0,100,0" o="]]..colors[16]..[[" c="4"/><S T="12" X="400" Y="200" L="400" H="400" P="1,0,0,0,80,0,100,0" o="]]..colors[17]..[[" c="4"/><S T="12" X="400" Y="200" L="325" H="325" P="1,0,0,0,85,0,100,0" o="]]..colors[18]..[[" c="4"/><S T="12" X="400" Y="200" L="250" H="250" P="1,0,0,0,90,0,100,0" o="]]..colors[19]..[[" c="4"/><S T="12" X="400" Y="200" L="175" H="175" P="1,0,0,0,95,0,100,0" o="]]..colors[20]..[[" c="4"/><S T="12" X="400" Y="200" L="100" H="100" P="1,0,0,0,100,0,100,0" o="]]..colors[21]..[[" c="4"/></S><D/><O/><L><JR M1="2" P1="400,200" MV="Infinity,]]..velocities[1]..[["/><JR M1="3" P1="400,200" MV="Infinity,]]..velocities[2]..[["/><JR M1="4" P1="400,200" MV="Infinity,]]..velocities[3]..[["/><JR M1="5" P1="400,200" MV="Infinity,]]..velocities[4]..[["/><JR M1="6" P1="400,200" MV="Infinity,]]..velocities[5]..[["/><JR M1="7" P1="400,200" MV="Infinity,]]..velocities[6]..[["/><JR M1="8" P1="400,200" MV="Infinity,]]..velocities[7]..[["/><JR M1="9" P1="400,200" MV="Infinity,]]..velocities[8]..[["/><JR M1="10" P1="400,200" MV="Infinity,]]..velocities[9]..[["/><JR M1="11" P1="400,200" MV="Infinity,]]..velocities[10]..[["/><JR M1="12" P1="400,200" MV="Infinity,]]..velocities[11]..[["/><JR M1="13" P1="400,200" MV="Infinity,]]..velocities[12]..[["/><JR M1="14" P1="400,200" MV="Infinity,]]..velocities[13]..[["/><JR M1="15" P1="400,200" MV="Infinity,]]..velocities[14]..[["/><JR M1="16" P1="400,200" MV="Infinity,]]..velocities[15]..[["/><JR M1="17" P1="400,200" MV="Infinity,]]..velocities[16]..[["/><JR M1="18" P1="400,200" MV="Infinity,]]..velocities[17]..[["/><JR M1="19" P1="400,200" MV="Infinity,]]..velocities[18]..[["/><JR M1="20" P1="400,200" MV="Infinity,]]..velocities[19]..[["/><JR M1="21" P1="400,200" MV="Infinity,]]..velocities[20]..[["/></L></Z></C>]]
 
 tfm.exec.disableAutoNewGame()
 tfm.exec.disableAfkDeath()
+tfm.exec.disableMortCommand()
+tfm.exec.disablePhysicalConsumables()
 tfm.exec.disableAutoShaman()
 tfm.exec.newGame(map)
+
+ui.setMapName("Aykv")
+
+function eventNewPlayer(p)
+	tfm.exec.respawnPlayer(p)
+end
+
+function eventPlayerDied(p)
+	tfm.exec.respawnPlayer(p)
+end
