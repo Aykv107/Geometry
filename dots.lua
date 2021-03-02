530e317347bc2197bec47a6c6d93e5502e18e5e4
Aykv107 <48999217+Aykv107@users.noreply.github.com>
2021-03-02 19:08:58 -0300

    Update dots.lua

--- dots.lua
+++ dots.lua
@@ -15,29 +15,84 @@ joints = [[]]
 velocities = {}
 colors = {}
 
+function rgb_hex(r, g, b)
+	local base16 = {
+		[0] = "0",
+		[1] = "1",
+		[2] = "2",
+		[3] = "3",
+		[4] = "4",
+		[5] = "5",
+		[6] = "6",
+		[7] = "7",
+		[8] = "8",
+		[9] = "9",
+		[10] = "A",
+		[11] = "B",
+		[12] = "C",
+		[13] = "D",
+		[14] = "E",
+		[15] = "F"
+	}
+
+	local _r1 = math.floor(math.floor(r) / 16)
+	local _r2 = ((math.floor(r) / 16) - _r1) * 16
+
+	local _g1 = math.floor(math.floor(g) / 16)
+	local _g2 = ((math.floor(g) / 16) - _g1) * 16
+
+	local _b1 = math.floor(math.floor(b) / 16)
+	local _b2 = ((math.floor(b) / 16) - _b1) * 16
+
+	return base16[_r1] .. base16[_r2] .. base16[_g1] .. base16[_g2] .. base16[_b1] .. base16[_b2]
+end
+
+function colorControl(colors)
+	for i, color in next, colors do
+		if color > 255 then
+			colors[i] = 255
+		end
+	end
+
+	return colors
+end
+
 -------------------
 
 type = 13
 
-posX = 280
+posX = 250
 posY = 200
 jointPos = "400,200"
 jx = 400
 jy = 200
-width = 100
-height = 100
+width = 10
+height = 10
 
-color = "FFFFFF"
 v = -1
+
+r = 255
+g = 255
+b = 255
+
 -------------------
 
-for i = 2, 52 do
+for i = 2, 42 do
+	local filter = colorControl({math.abs(r), math.abs(g), math.abs(b)})
+	local color = rgb_hex(table.unpack(filter))
+
 	jointPos = tostring(jx) .. "," .. tostring(jy)
 	grounds = grounds .. [[<S T="]]..type..[[" X="]]..posX..[[" Y="]]..posY..[[" L="]]..width..[[" H="]]..height..[[" P="1,0,0.3,0.2,0,0,100,0" o="]]..color..[[" c="4"/>]]
 	joints = joints .. [[<JR M1="]]..i..[[" P1="]]..jointPos..[[" MV="Infinity,]]..v..[["/>]]
-	v = v - 0.01
-	--posX = posX + 10
+	v = v - 0.1
+	--posX = posX + 15
+	--posY = posY - 7
 	--jx = jx + 15
+	--jy = jy + 7
+
+	r = r - 20
+	g = g - 12
+	b = b - 8
 end
 
 -------------------
